bc895ae425afd39513d6adeb04cc336772f033cb
Chris Park <cpark@basistech.com>
2017-12-21 09:24:34 -0500

    WS-1272 Multilingual example

---
+++ examples/language_multilingual.R
@@ -0,0 +1,27 @@
+source("../R/Api.R")
+library(jsonlite)
+library("optparse")
+
+option_list = list( make_option(c("-k", "--key"), action="store", default=NA, type='character',
+              help="Rosette API key"), make_option(c("-u", "--url"), action="store", default=NA, type='character',
+              help="Rosette API url"))
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+language_data <- "TBD"
+
+options <- list()
+options[[ "multilingual" ]] <- "true"
+
+parameters <- list()
+parameters[[ "content" ]] <- language_data
+parameters[[ "options" ]] <- options
+custom_headers <- c("X-RosetteAPI-App" = "r-application-test")
+
+if (is.na(opt$url)){
+  result <- api(opt$key, "language", parameters = parameters, custom_headers = custom_headers)
+} else {
+  result <- api(opt$key, "language", parameters = parameters, custom_headers = custom_headers, NULL, url = opt$url)
+}
+print(jsonlite::toJSON(result$header, pretty = TRUE))
+print(jsonlite::toJSON(result$content, pretty = TRUE))
