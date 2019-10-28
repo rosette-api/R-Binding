a821779c7777410c5f3ed2e634c4293bf1f87ca7
Seth Gransky <seth@basistech.com>
2019-10-28 16:56:00 -0500

    DEVOPS-247:  Fitting with scripts.  Rollback bad update.

--- R/Api.R
+++ R/Api.R
@@ -147,8 +147,8 @@ api <- function(user_key, endpoint, parameters=FALSE, custom_headers=NULL,
 #' Provides the binding Version
 #'
 #' @return current binding version
-get_binding_version <- "1.14.3"
-  binding_version <- "1.14.3"
+get_binding_version <- function() {
+  binding_version <- "_new_version_"
   return(binding_version)
 }
 
